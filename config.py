a38113c46ae741a23d1a41475d0704fb4f29d47b
Baddest008 <victor.ilesanmi23@gmail.com>
2025-02-09 19:41:44 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Set your OpenAI API Key here
+os.environ["OPENAI_API_KEY"] = "your_openai_api_key_here"
+
+# Set your ElevenLabs API Key here
+os.environ["ELEVEN_API_KEY"] = "your_elevenlabs_api_key_here"
